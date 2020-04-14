86ba64aaf9b0fe9ea93e6f81bbc4976f5afc1470
Julio Muller <juliodolago@gmail.com>
2020-04-14 09:54:12 -0300

    Created layout components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <v-app>
+    <app-header />
     <v-content>
       <v-container>
         <router-view />
       </v-container>
     </v-content>
+    <app-footer />
   </v-app>
 </template>
 
 <script>
-export default {}
+import AppHeader from '@/components/layout/Header'
+import AppFooter from '@/components/layout/Footer'
+
+export default {
+
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
 </script>
 
 <style>
