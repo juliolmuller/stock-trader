62f43c2f57bd99f8a6982d6f2f83379c901130b5
Julio Muller <juliodolago@gmail.com>
2020-04-14 01:10:25 -0300

    Project startup

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../components/views/Home.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: 'hash',
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../components/views/About.vue')
+    }
+  ]
+})
