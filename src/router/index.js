1d24448f30c55b3651d1f14febf86575ce4921de
Julio Muller <juliodolago@gmail.com>
2020-04-14 09:46:44 -0300

    Created base views and routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '@/components/Home'
+import Portfolio from '@/components/portfolio/PortfolioPanel'
+import Stocks from '@/components/stocks/StocksPanel'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
   mode: 'hash',
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/portfolio',
+      name: 'portfolio',
+      component: Portfolio
+    },
+    {
+      path: '/stocks',
+      name: 'stocks',
+      component: Stocks
+    },
+    {
+      // fallback route
+      path: '*',
+      redirect: '/'
+    }
+  ]
 })
