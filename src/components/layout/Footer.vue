cfe9d9c8088e01b3a6dd753a0a9f379ba9142d6c
Julio Muller <juliodolago@gmail.com>
2020-04-14 23:48:40 -0300

    Finished footer component

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,9 +1,36 @@
 <template>
-  <h1>Rodapé</h1>
+  <v-app-bar app bottom class="d-flex justify-center align-center">
+    <span class="caption">
+      Todos os direitos reservados &copy; 2020
+      <span class="font-weight-black">LacusSoft</span>
+    </span>
+
+    <v-tooltip top v-for="icon in icons" :key="icon.name">
+      <template v-slot:activator="{ on }">
+        <v-btn icon :href="icon.href" v-on="on" class="mr-8">
+          <v-icon>{{ icon.name }}</v-icon>
+        </v-btn>
+      </template>
+      <span>{{ icon.tooltip }}</span>
+    </v-tooltip>
+  </v-app-bar>
 </template>
 
 <script>
-export default {}
+export default {
+
+  data() {
+    return {
+      icons: [
+        {
+          href: 'https://github.com/juliolmuller/stock-trader',
+          tooltip: 'Veja no GitHub',
+          name: 'mdi-github'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style scoped>
