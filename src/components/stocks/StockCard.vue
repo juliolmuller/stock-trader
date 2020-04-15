d8f0adbd31deb921c03e4acb64902f4dcd4bfcfe
Julio Muller <juliodolago@gmail.com>
2020-04-15 00:35:59 -0300

    Created card to display stocks

---
+++ src/components/stocks/StockCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-flex class="pr-5 pb-5" xs12 md6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="d-flex justify-space-between">
+        <h3 class="order-2">{{ stock.company }}</h3>
+        <span class="order-1">{{ stock.id }}</span>
+        <span class="order-3">${{ stock.price }} por ação</span>
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field type="number" label="Quantidade" />
+        <v-btn class="green darken-3 white--text">Comprar</v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+
+  props: {
+    stock: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/components/stocks/StocksPanel.vue
+++ src/components/stocks/StocksPanel.vue
@@ -1,9 +1,34 @@
 <template>
-  <h1>Painel de Ações</h1>
+  <v-layout row wrap class="pt-10">
+    <StockCard
+      v-for="stock in stocks"
+      :key="stock.id"
+      :stock="stock"
+    />
+  </v-layout>
 </template>
 
 <script>
-export default {}
+import StockCard from './StockCard'
+
+export default {
+
+  components: {
+    StockCard
+  },
+
+  data() {
+    return {
+      stocks: [
+        { id: 'BMW3', company: 'NMW', price: 20.57 },
+        { id: 'AMZ4', company: 'Amazon', price: 8.87 },
+        { id: 'GOO2', company: 'Google', price: 11.20 },
+        { id: 'FBK1', company: 'Facebook', price: 5.51 },
+        { id: 'XOM1', company: 'ExxonMobil', price: 54.32 }
+      ]
+    }
+  }
+}
 </script>
 
 <style scoped>
